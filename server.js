f8f4a9cc5b9522fa73f031d11e1cbc4ac899e502
OnkarJondhale <onkar.jondhale@gmail.com>
2025-06-07 15:10:42 +0530

    Fixed cors issue

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 4000;
 app.use(cookieParser());
 app.use(cors(
     {
-        origin: 'http://localhost:5173', 
+        origin: '*', 
         credentials: true,
     }
 ));
